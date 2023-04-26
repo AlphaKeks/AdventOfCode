ab17a755423e335135c00a312c80c9da1c41c36a
AlphaKeks <alphakeks@dawn.sh>
2023-04-26 16:32:33 +0200

    Rename things for Day 5

--- 2022/src/day5/mod.rs
+++ 2022/src/day5/mod.rs
@@ -1,13 +1,11 @@
-use {
-	advent_of_code::AdventOfCode, anyhow::Result, itertools::Itertools, std::collections::VecDeque,
-};
+use {advent_of_code::AdventOfCode, anyhow::Result, itertools::Itertools};
 
 #[derive(Default)]
 pub struct AoC {
 	input: &'static str,
 }
 
-type Stack = VecDeque<char>;
+type Column = Vec<char>;
 type Instructions = (usize, usize, usize);
 
 impl AoC {
@@ -15,40 +13,40 @@ impl AoC {
 		Self { input }
 	}
 
-	fn parse(&self) -> Result<(Vec<Stack>, Vec<Instructions>)> {
+	fn parse(&self) -> Result<(Vec<Column>, Vec<Instructions>)> {
 		let (crates, instructions) = self
 			.input
 			.split_once("\n\n")
 			.expect("Expected 2 sections: crates and instructions");
 
-		let mut crate_stacks = Vec::new();
+		let mut columns = Vec::new();
 
 		for row in crates
 			.lines()
 			.map(|row| row.chars().collect_vec())
 		{
-			for (idx, stack_crate) in row.chunks(4).enumerate() {
+			for (idx, cell) in row.chunks(4).enumerate() {
 				// e.g. `[N] `
-				let stack_crate = stack_crate[1];
+				let cell = cell[1];
 
-				match crate_stacks.get_mut(idx) {
+				match columns.get_mut(idx) {
 					// We are still in the first row and have found a crate
-					None if stack_crate.is_alphabetic() => {
-						crate_stacks.push(VecDeque::from_iter([stack_crate]));
+					None if cell.is_alphabetic() => {
+						columns.push(Vec::from_iter([cell]));
 					}
 
 					// We are still in the first row and have found an empty spot
 					None => {
-						crate_stacks.push(VecDeque::new());
+						columns.push(Vec::new());
 					}
 
 					// We are not in the first row anymore and have found a crate.
-					Some(stack) if stack_crate.is_alphabetic() => {
-						stack.push_back(stack_crate);
+					Some(column) if cell.is_alphabetic() => {
+						column.push(cell);
 					}
 
 					// We are not in the first row anymore, but we do not have a crate to push onto the
-					// stack.
+					// column.
 					_ => {}
 				};
 			}
@@ -66,7 +64,7 @@ impl AoC {
 			})
 			.collect_vec();
 
-		Ok((crate_stacks, instructions))
+		Ok((columns, instructions))
 	}
 }
 
@@ -78,37 +76,37 @@ impl AdventOfCode for AoC {
 	const DAY: usize = 5;
 
 	fn part_a(&mut self) -> Result<Self::SolutionA> {
-		let (mut stacks, instructions) = self.parse()?;
+		let (mut columns, instructions) = self.parse()?;
 		for (count, from, to) in instructions {
 			for _ in 0..count {
-				let crate_in_question = stacks[from].pop_front().unwrap();
-				stacks[to].push_front(crate_in_question);
+				let le_crate = columns[from].remove(0);
+				columns[to].insert(0, le_crate);
 			}
 		}
 
-		Ok(stacks
+		Ok(columns
 			.into_iter()
-			.map(|stack| stack[0])
+			.map(|column| column[0])
 			.collect())
 	}
 
 	fn part_b(&mut self) -> Result<Self::SolutionB> {
-		let (mut stacks, instructions) = self.parse()?;
+		let (mut columns, instructions) = self.parse()?;
 		for (count, from, to) in instructions {
-			let mut crates_in_question = stacks[from]
+			let mut crates = columns[from]
 				.drain(..count)
 				.collect_vec();
 
-			crates_in_question.reverse();
+			crates.reverse();
 
-			for crate_in_question in crates_in_question {
-				stacks[to].push_front(crate_in_question);
+			for le_crate in crates {
+				columns[to].insert(0, le_crate);
 			}
 		}
 
-		Ok(stacks
+		Ok(columns
 			.into_iter()
-			.map(|stack| stack[0])
+			.map(|column| column[0])
 			.collect())
 	}
 }
